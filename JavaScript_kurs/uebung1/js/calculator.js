cc78b4d12f174bbf06373fe86db1bc71bb4ace47
Veronika Lackner <vroni.lackner@hotmail.com>
2019-01-23 21:12:51 +0100

    23.01.2019 - Calulator finished

--- JavaScript_kurs/uebung1/js/calculator.js
+++ JavaScript_kurs/uebung1/js/calculator.js
@@ -1,7 +1,42 @@
-$('input[type="button"]').click(function () {
+/*$('input[type="button"]').click(function () {
   $('input.calc').val(
     $('input.calc').val() + this.value
   )
 }
+)*/
 
-)
+$('input').on('keyup', function () {
+  if($('.calc').val() != $('.calc').val($('.calc').val()+$(this).val()))
+    $('.calc').val('');
+});
+
+$('input.number').on('click', function () {
+    if($(this).attr('input') != '.operator') {
+        $('.calc').val($('.calc').val()+$(this).val());
+    }
+   else {
+     if($('.calc').val()!='') {
+       $('.calc').val($('.calc').val()+$(this).val());
+     }
+   }
+});
+
+$('input.operator').on('click', function () {
+    if($(this).attr('input') != '.number') {
+        $('.calc').val($('.calc').val()+$(this).val());
+    }
+   else {
+     if($('.calc').val()!='') {
+       $('.calc').val($('.calc').val()+$(this).val());
+     }
+   }
+});
+
+$('input.empty').on('click', function () {
+  $('.calc').val('');
+});
+
+$('input.value').on('click', function () {
+  var ergebnis = $('.calc').val();
+  $('.calc').val(eval(ergebnis));
+});
